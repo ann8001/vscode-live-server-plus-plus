2eae923a0e2070cf01d349a45a785c2065193d64
ann8001 <copybyannp@gmail.com>
2025-07-08 17:41:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-2">
+<meta http-equiv="Content-Language" content="pl">
+<meta name="robots" content="index,follow">
+<title>Apteka Zdrowie - Staszów</title>
+<meta name="description" content="Apteka Zdrowie w Staszowie. Apteka ze 10-letnią tradycją!  Apteka oferuje tańsze zamienniki drogich leków.">
+<meta name="keywords" content="apteka staszów, apteki zdrowie, leki, leki robione, tanie leki, apteka internetowa staszów, tańsze zamienniki leków">
+<link href="s/styl.css?ver=1.0" rel="stylesheet" type="text/css">
+<link rel="shortcut icon" href="/i/favicon.ico" type="image/x-icon">
+<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<script src="/dist/slippry.min.js"></script>
+<script src="//use.edgefonts.net/cabin;source-sans-pro:n2,i2,n3,n4,n6,n7,n9.js"></script>
+<link rel="stylesheet" href="demo.css">
+<link rel="stylesheet" href="/dist/slippry.css">
+</head>
+
+<body oncopy="return false">
+<style>
+<!--
+input{background-color:#ffffff; border-color:#cccccc; border-style:solid; color:#000077;}
+#warstwa1{left:50%; margin-top:260px; margin-left:-330px;width:640px; height:420px; position:absolute; visibility:visible;z-index:600}
+
+-->
+</style>
+<script language="JavaScript">
+<!--
+var pnn=(document.layers)?true:false;
+var pn6=(navigator.vendor == ("Netscape6") || navigator.product == ("Gecko"));
+var pie=false;
+var pop=false;
+if (document.all){ if (navigator.userAgent.search('Opera')>=0){ pie=false; pop=true;} else {pie=true; pop=false;}}
+
+function pokaz(warstwa)
+	{
+	if(pnn)	{ document.layers[warstwa].visibility="show"; } else
+	if(pie || pop)	{ document.all[warstwa].style.visibility="visible"; } else
+	if(pn6)	{ document.getElementById(warstwa).style.visibility="visible"; }
+	}
+
+function ukryj(warstwa)
+	{
+	if(pnn)	{ document.layers[warstwa].visibility="hide"; } else
+	if(pie || pop)	{ document.all[warstwa].style.visibility="hidden"; } else
+	if(pn6)	{ document.getElementById(warstwa).style.visibility="hidden"; }
+	}
+
+//-->
+</script> 
+<!--<div id="warstwa1" style="position:absolute;z-index:5">
+  <a href="#" name="ukryj1" value="ukryj warstwe 1" onclick="ukryj('warstwa1');" title="zamknij okno"><img src="/i/card.png" alt="zamknij okno" /></a>-->
+
+</div>
+<div id="all">
+  <div id="container">
+    <div class="header">
+      	<div class="logo"> <a href="https://recepta.pl/" target="_blank" title="Apteka internetowa"><h2>Apteka Zdrowie - Staszów</h2></a>
+            
+             <a href="http://apteline.pl" target="_blank" title="Apteka internetowa"><h3>Apteka internetowa</h3></a>
+            </div>
+
+      <div class="menu">
+        <ul>
+          <li><a href="/o-nas/" title="O nas" class="m2">O nas</a> 
+           
+          </li>
+          <li><a href="/porady-i-informacje/" title="Porady i informacje" class="m3">Porady i informacje</a>
+            <ul>
+              <li><a href="/porady-i-informacje/informacje-dla-pacjentow/" 
+                                    title="Informacja dla Pacjenta ">Informacja dla Pacjenta</a></li>
+              <li><a href="/porady-i-informacje/e-recepta/" 
+                                    title="E RECEPTA">E RECEPTA</a></li>
+              <li><a href="/porady-i-informacje/e-pacjent/" 
+                                    title="E PACJENT">E PACJENT</a></li>
+              <li><a href="/porady-i-informacje/sfalszowane-leki-zabijaja/" 
+                                    title="Sfałszowane leki zabijają" rel="nofollow">Sfałszowane leki zabijają</a></li>
+            
+            </ul>
+          </li>
+          <li><a href="/wybrane-oferty/" title="Wybrane oferty" class="m4">Wybrane oferty</a>
+            <ul>
+             <li><a href="/wybrane-oferty/dermokosmetyki-i-kosmetyki-profesjonalne-dr-irena-eris/" 
+                                    title="Dermokosmetyki i kosmetyki profesjonalne dr Eris">Dermokosmetyki i kosmetyki Dr Irena Eris</a></li>
+              <li><a href="/wybrane-oferty/odzywki-sportowe-olimp/" title="Odżywki sportowe Olimp">Odżywki sportowe Olimp</a></li>
+				<li><a href="/wybrane-oferty/suplementy-witaminy-solgar/" title="Suplementy i witaminy Solgar">Suplementy i witaminy Solgar</a></li>
+            </ul>
+          </li>
+          <li><a href="/zadaj-pytanie/" title="Zadaj pytanie / Zamów lek" class="m5">Zadaj pytanie </a></li>
+          <li><a href="/dyzury-aptek/" 
+                        title="Dyżury Aptek" class="m6">Dyżury Aptek</a> 
+           
+          </li>
+          <li><a href="/lekarze-specjalisci/" title="Lekarze specjaliści w Staszowie"  class="m7">Lekarze w Staszowie</a></li>
+            <li><a href="/zakupy-przez-internet-odbior-osobisty/" target="_blank" title="Zakupy przez internet. Bezpłatny odbiór w Aptece Zdrowie ul. Opatowska 10" class="m8">Zakupy przez internet</a></li>
+          <li><a href="/kontakt/" title="Kontakt"  class="m9">Kontakt</a></li>
+        </ul>
+      </div>
+      
+      
+      <?php readfile('include/header.php'); ?> 
+      
+      
+    
+    </div>
+    <div class="content"> <?php echo iconv('utf-8', 'iso8859-2', file_get_contents('easyedit/contents/homepage.html')); ?> </div>
+    <div class="clear"></div>
+  </div>
+</div>
+
+
+<?php readfile('include/footer.php'); ?> 
+
+
+</body>
+</html>
